7bd91ea180abb78f5193be5037e5303d0e8e640d
Saish Gersappa <sgersapp@cisco.com>
2016-08-24 21:41:28 -0700

    cleanup
    Change-Id: I200aef06bc61969e21443f2319b6f9efc3f3957a

--- find_available_room.py
+++ find_available_room.py
@@ -1,67 +1,85 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+import csv
+import datetime
+import getpass
+import subprocess
 import sys
+import xml.etree.ElementTree as ET
+
+from string import Template
+
+URL = 'https://mail.cisco.com/ews/exchange.asmx'
+SCHEME_TYPES = './/{http://schemas.microsoft.com/exchange/services/2006/types}'
+TIME_NOW = datetime.datetime.now().replace(microsecond=0).isoformat()
+TIME_1H_FROM_NOW = None
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-import subprocess
-import getpass
-from string import Template
-import xml.etree.ElementTree as ET
-import csv, codecs
-import argparse
-import datetime
+def read_room_list(filename='rooms.csv'):
+    rooms = {}
 
-now = datetime.datetime.now().replace(microsecond=0)
-starttime_default = now.isoformat()
-end_time_default = None
+    with open(filename, 'r') as fhandle:
+        reader = csv.reader(fhandle)
+        for row in reader:
+            rooms[unicode(row[1])] = unicode(row[0])
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-url","--url", help="url for exhange server, e.g. 'https://mail.domain.com/ews/exchange.asmx'.",required=True)
-parser.add_argument("-u","--user", help="user name for exchange/outlook",required=True)
-parser.add_argument("-start","--starttime", help="Starttime e.g. 2014-07-02T11:00:00 (default = now)", default=starttime_default)
-parser.add_argument("-end","--endtime", help="Endtime e.g. 2014-07-02T12:00:00 (default = now+1h)", default=end_time_default)
-#parser.add_argument("-n","--now", help="Will set starttime to now and endtime to now+1h", action="store_true")
-parser.add_argument("-f","--file", help="csv filename with rooms to check (default=rooms.csv). Format: Name,email",default="rooms.csv")
+    return rooms
 
-args=parser.parse_args()
 
-url = args.url
+def find_available_rooms(rooms, user, password, start_time=TIME_NOW, end_time=None):
+    if end_time is None:
+        start = datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S")
+        end_time = (start + datetime.timedelta(hours=1)).isoformat()
 
-rooms={}
-reader = csv.reader(codecs.open(args.file, 'r', encoding='utf-8')) 
-for row in reader: 
-	rooms[unicode(row[1])]=unicode(row[0])
+    print "Searching for a room from " + start_time + " to " + end_time + ":"
+    print "{0:10s} {1:64s} {2:64s}".format("Status", "Room", "Email")
 
-start_time = args.starttime
-if not args.endtime:
-	start = datetime.datetime.strptime( start_time, "%Y-%m-%dT%H:%M:%S" )
-	end_time = (start + datetime.timedelta(hours=1)).isoformat()
-else:
-	end_time = args.endtime
+    xml_template = open("getavailibility_template.xml", "r").read()
+    xml = Template(xml_template)
+    for room in rooms:
+        data = unicode(xml.substitute(email=room, starttime=start_time, endtime=end_time))
 
-user = args.user
-password = getpass.getpass("Password:")
+        header = "\"content-type: text/xml;charset=utf-8\""
+        command = "curl --silent --header " + header \
+                   + " --data '" + data \
+                   + "' --ntlm " \
+                   + "-u "+ user + ":" + password \
+                   + " " + URL
+        response = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
 
-print "Searching for a room from " + start_time + " to " + end_time + ":"
-print "{0:10s} {1:64s} {2:64s}".format("Status", "Room", "Email")
+        tree = ET.fromstring(response)
 
-xml_template = open("getavailibility_template.xml", "r").read()
-xml = Template(xml_template)
-for room in rooms:
-	data = unicode(xml.substitute(email=room,starttime=start_time,endtime=end_time))
+        status = "Free"
+        # arrgh, namespaces!!
+        elems = tree.findall(SCHEME_TYPES + "BusyType")
+        for elem in elems:
+            status = elem.text
 
-	header = "\"content-type: text/xml;charset=utf-8\""
-	command = "curl --silent --header " + header +" --data '" + data + "' --ntlm "+"--negotiate "+ "-u "+ user+":"+password+" "+ url
-	response = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
+        print "{0:10s} {1:64s} {2:64s}".format(status, rooms[room], room)
 
-	tree = ET.fromstring(response)
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--user", help="user name for exchange/outlook", required=True)
+    parser.add_argument("-start", "--starttime",
+                        help="Starttime e.g. 2014-07-02T11:00:00 (default = now)",
+                        default=TIME_NOW)
+    parser.add_argument("-end", "--endtime",
+                        help="Endtime e.g. 2014-07-02T12:00:00 (default = now+1h)",
+                        default=TIME_1H_FROM_NOW)
+    parser.add_argument("-f", "--file",
+                        help="csv filename with room info (default=rooms.csv).",
+                        default="rooms.csv")
 
-	status = "Free"
-	# arrgh, namespaces!!
-	elems=tree.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}BusyType")
-	for elem in elems:
-		status=elem.text
+    args = parser.parse_args()
+    args.password = getpass.getpass("Password:")
 
-	print "{0:10s} {1:64s} {2:64s}".format(status, rooms[room], room)
+    rooms = read_room_list()
+    find_available_rooms(rooms, args.user, args.password, args.starttime, args.endtime)
+
+
+if __name__ == '__main__':
+    run()
