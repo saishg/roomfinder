70e2161f699220ed8f0cae82d65e97ac2ce0c5f4
Saish Gersappa <sgersapp@cisco.com>
2016-09-04 10:47:17 -0700

    sgersapp: Add tests for room reservation
    Change-Id: I95e15e2ed2ad0a0ca246a8d4a7bda4e8f39a0d3e

--- test_exchange_api.py
+++ test_exchange_api.py
@@ -5,6 +5,15 @@ import string
 
 ROOM_AVAIL_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><h:ServerVersionInfo MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2" xmlns:h="http://schemas.microsoft.com/exchange/services/2006/types" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><GetUserAvailabilityResponse xmlns="http://schemas.microsoft.com/exchange/services/2006/messages"><FreeBusyResponseArray><FreeBusyResponse><ResponseMessage ResponseClass="Success"><ResponseCode>NoError</ResponseCode></ResponseMessage><FreeBusyView><FreeBusyViewType xmlns="http://schemas.microsoft.com/exchange/services/2006/types">DetailedMerged</FreeBusyViewType><MergedFreeBusy xmlns="http://schemas.microsoft.com/exchange/services/2006/types">$freebusy</MergedFreeBusy><WorkingHours xmlns="http://schemas.microsoft.com/exchange/services/2006/types"><TimeZone><Bias>480</Bias><StandardTime><Bias>0</Bias><Time>02:00:00</Time><DayOrder>1</DayOrder><Month>11</Month><DayOfWeek>Sunday</DayOfWeek></StandardTime><DaylightTime><Bias>-60</Bias><Time>02:00:00</Time><DayOrder>2</DayOrder><Month>3</Month><DayOfWeek>Sunday</DayOfWeek></DaylightTime></TimeZone><WorkingPeriodArray><WorkingPeriod><DayOfWeek>Sunday Monday Tuesday Wednesday Thursday Friday Saturday</DayOfWeek><StartTimeInMinutes>0</StartTimeInMinutes><EndTimeInMinutes>1439</EndTimeInMinutes></WorkingPeriod></WorkingPeriodArray></WorkingHours></FreeBusyView></FreeBusyResponse></FreeBusyResponseArray></GetUserAvailabilityResponse></s:Body></s:Envelope>')
 
+ROOM_RESERVE_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><h:ServerVersionInfo xmlns:h="http://schemas.microsoft.com/exchange/services/2006/types" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2" Version="V2_23"/></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><m:CreateItemResponse xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"><m:ResponseMessages><m:CreateItemResponseMessage ResponseClass="$result"><m:ResponseCode>NoError</m:ResponseCode><m:Items><t:CalendarItem><t:ItemId Id="AAMkADdjOTY0YWI5LWI4NzAtNGU4OC04MjAyLTgyMjFjZmFlMzhkOQBGAAAAAAA537+zip9BToctZGZWgs3IBwD/dt2bgtXgRKEhWftUlaSuAAAC3uxpAADFT7UXKH2LTLWQ1hNAo69lAAC3z5RCAAA=" ChangeKey="DwAAABYAAADFT7UXKH2LTLWQ1hNAo69lAAC34R9T"/></t:CalendarItem></m:Items></m:CreateItemResponseMessage></m:ResponseMessages></m:CreateItemResponse></s:Body></s:Envelope>')
+
+ROOM_EMAIL = 'ROOM@example.com'
+ROOM_NAME = 'ROOM'
+START_TIME = "2016-07-02T11:00:00"
+END_TIME = "2016-07-02T11:00:00"
+TIME_ZONE_OFFSET = "480"
+
+
 def get_popen_mock_room_avail(mock_popen, freebusy):
     process_mock = mock.Mock()
     attrs = {'communicate.return_value': (ROOM_AVAIL_XML.substitute(freebusy=freebusy), '')}
@@ -22,7 +31,6 @@ def test_auth_failure(mock_popen):
 @mock.patch('exchange_api.subprocess.Popen')
 def test_room_available(mock_popen):
     FREEBUSY = '0000'
-    ROOM_EMAIL = 'ROOM@example.com'
     get_popen_mock_room_avail(mock_popen, FREEBUSY)
 
     api = exchange_api.ExchangeApi(user="testuser", password="testpassword")
@@ -39,18 +47,53 @@ def test_room_available(mock_popen):
 @mock.patch('exchange_api.subprocess.Popen')
 def test_room_unavailable(mock_popen):
     FREEBUSY = '0022'
-    ROOM_EMAIL = 'ROOM@example.com'
     get_popen_mock_room_avail(mock_popen, FREEBUSY)
 
     api = exchange_api.ExchangeApi(user="testuser", password="testpassword")
     response = api.room_status(room_email=ROOM_EMAIL,
-                               start_time="2014-07-02T11:00:00",
-                               end_time="2014-07-02T11:00:00",
+                               start_time=START_TIME,
+                               end_time=END_TIME,
                                timezone_offset="480")
 
     # Keys:'freebusy', 'status', 'email'
     assert response['status'] != 'Free'
     assert response['freebusy'] == FREEBUSY
     assert response['email'] == ROOM_EMAIL
+
+
+def get_popen_mock_room_reserve(mock_popen, result):
+    process_mock = mock.Mock()
+    attrs = {'communicate.return_value': (ROOM_RESERVE_XML.substitute(result=result), '')}
+    process_mock.configure_mock(**attrs)
+    mock_popen.return_value = process_mock 
+
+@mock.patch('exchange_api.subprocess.Popen')
+def test_room_reserve_success(mock_popen):
+    RESULT = 'Success'
+    get_popen_mock_room_reserve(mock_popen, RESULT)
+
+    api = exchange_api.ExchangeApi(user="testuser", password="testpassword")
+    response = api.reserve_room(room_email=ROOM_EMAIL,
+                                room_name=ROOM_NAME,
+                                start_time=START_TIME,
+                                end_time=END_TIME,
+                                timezone_offset=TIME_ZONE_OFFSET)
+
+    assert response is True
+
+@mock.patch('exchange_api.subprocess.Popen')
+def test_room_reserve_failure(mock_popen):
+    RESULT = 'Failure'
+    get_popen_mock_room_reserve(mock_popen, RESULT)
+
+    api = exchange_api.ExchangeApi(user="testuser", password="testpassword")
+    response = api.reserve_room(room_email=ROOM_EMAIL,
+                                room_name=ROOM_NAME,
+                                start_time=START_TIME,
+                                end_time=END_TIME,
+                                timezone_offset=TIME_ZONE_OFFSET)
+
+    assert response is False
+
 
 
