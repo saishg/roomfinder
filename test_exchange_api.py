cdb3efe381fafd6a9e6c59f3719ef33719bc56f3
Saish Gersappa <sgersapp@cisco.com>
2016-09-04 11:50:30 -0700

    sgersapp: Unit tests for finding rooms
    Change-Id: I5f39599d38e588a2755b51fc8a0da336c2c9537b

--- test_exchange_api.py
+++ test_exchange_api.py
@@ -3,30 +3,62 @@ import mock
 import nose
 import string
 
-ROOM_AVAIL_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><h:ServerVersionInfo MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2" xmlns:h="http://schemas.microsoft.com/exchange/services/2006/types" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><GetUserAvailabilityResponse xmlns="http://schemas.microsoft.com/exchange/services/2006/messages"><FreeBusyResponseArray><FreeBusyResponse><ResponseMessage ResponseClass="Success"><ResponseCode>NoError</ResponseCode></ResponseMessage><FreeBusyView><FreeBusyViewType xmlns="http://schemas.microsoft.com/exchange/services/2006/types">DetailedMerged</FreeBusyViewType><MergedFreeBusy xmlns="http://schemas.microsoft.com/exchange/services/2006/types">$freebusy</MergedFreeBusy><WorkingHours xmlns="http://schemas.microsoft.com/exchange/services/2006/types"><TimeZone><Bias>480</Bias><StandardTime><Bias>0</Bias><Time>02:00:00</Time><DayOrder>1</DayOrder><Month>11</Month><DayOfWeek>Sunday</DayOfWeek></StandardTime><DaylightTime><Bias>-60</Bias><Time>02:00:00</Time><DayOrder>2</DayOrder><Month>3</Month><DayOfWeek>Sunday</DayOfWeek></DaylightTime></TimeZone><WorkingPeriodArray><WorkingPeriod><DayOfWeek>Sunday Monday Tuesday Wednesday Thursday Friday Saturday</DayOfWeek><StartTimeInMinutes>0</StartTimeInMinutes><EndTimeInMinutes>1439</EndTimeInMinutes></WorkingPeriod></WorkingPeriodArray></WorkingHours></FreeBusyView></FreeBusyResponse></FreeBusyResponseArray></GetUserAvailabilityResponse></s:Body></s:Envelope>')
+XML_KEYS = {'XML_SOAP_ENVELOPE' : 'http://schemas.xmlsoap.org/soap/envelope/',
+            'XML_SCHEMA' : 'http://schemas.microsoft.com/exchange/services/2006',
+            'XML_W3_SCHEMA' : 'http://www.w3.org/2001/XMLSchema',
+           }
 
-ROOM_RESERVE_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><h:ServerVersionInfo xmlns:h="http://schemas.microsoft.com/exchange/services/2006/types" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2" Version="V2_23"/></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><m:CreateItemResponse xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"><m:ResponseMessages><m:CreateItemResponseMessage ResponseClass="$result"><m:ResponseCode>NoError</m:ResponseCode><m:Items><t:CalendarItem><t:ItemId Id="AAMkADdjOTY0YWI5LWI4NzAtNGU4OC04MjAyLTgyMjFjZmFlMzhkOQBGAAAAAAA537+zip9BToctZGZWgs3IBwD/dt2bgtXgRKEhWftUlaSuAAAC3uxpAADFT7UXKH2LTLWQ1hNAo69lAAC3z5RCAAA=" ChangeKey="DwAAABYAAADFT7UXKH2LTLWQ1hNAo69lAAC34R9T"/></t:CalendarItem></m:Items></m:CreateItemResponseMessage></m:ResponseMessages></m:CreateItemResponse></s:Body></s:Envelope>')
+ROOM_INFO_XML = string.Template('<t:Resolution><t:Mailbox><t:Name>$room_name</t:Name><t:EmailAddress>$room_name@example.com</t:EmailAddress><t:RoutingType>SMTP</t:RoutingType><t:MailboxType>Mailbox</t:MailboxType></t:Mailbox><t:Contact><t:DisplayName>$room_name (12)</t:DisplayName><t:GivenName>ROOM</t:GivenName><t:Initials/><t:CompanyName/><t:EmailAddresses/><t:PhysicalAddresses/><t:PhoneNumbers/><t:AssistantName/><t:ContactSource>ActiveDirectory</t:ContactSource><t:Department/><t:JobTitle/><t:OfficeLocation/><t:Surname>$room_name</t:Surname></t:Contact></t:Resolution>')
+
+FIND_ROOMS_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="{XML_SOAP_ENVELOPE}"><s:Header><h:ServerVersionInfo xmlns:h="{XML_SCHEMA}/types" xmlns="{XML_SCHEMA}/types" xmlns:xsd="{XML_W3_SCHEMA}" xmlns:xsi="{XML_W3_SCHEMA}-instance" MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2"/></s:Header><s:Body xmlns:xsi="{XML_W3_SCHEMA}-instance" xmlns:xsd="{XML_W3_SCHEMA}"><m:ResolveNamesResponse xmlns:m="{XML_SCHEMA}/messages" xmlns:t="{XML_SCHEMA}/types"><m:ResponseMessages><m:ResolveNamesResponseMessage ResponseClass="Warning"><m:MessageText>Multiple results were found.</m:MessageText><m:ResponseCode>ErrorNameResolutionMultipleResults</m:ResponseCode><m:DescriptiveLinkKey>0</m:DescriptiveLinkKey><m:ResolutionSet TotalItemsInView="$num_rooms" IncludesLastItemInRange="true">$room_info</m:ResolutionSet></m:ResolveNamesResponseMessage></m:ResponseMessages></m:ResolveNamesResponse></s:Body></s:Envelope>'.format(**XML_KEYS))
+
+ROOM_AVAIL_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="{XML_SOAP_ENVELOPE}"><s:Header><h:ServerVersionInfo MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2" xmlns:h="{XML_SCHEMA}/types" xmlns="{XML_SCHEMA}/types" xmlns:xsd="{XML_W3_SCHEMA}" xmlns:xsi="{XML_W3_SCHEMA}-instance"/></s:Header><s:Body xmlns:xsi="{XML_W3_SCHEMA}-instance" xmlns:xsd="{XML_W3_SCHEMA}"><GetUserAvailabilityResponse xmlns="{XML_SCHEMA}/messages"><FreeBusyResponseArray><FreeBusyResponse><ResponseMessage ResponseClass="Success"><ResponseCode>NoError</ResponseCode></ResponseMessage><FreeBusyView><FreeBusyViewType xmlns="{XML_SCHEMA}/types">DetailedMerged</FreeBusyViewType><MergedFreeBusy xmlns="{XML_SCHEMA}/types">$freebusy</MergedFreeBusy><WorkingHours xmlns="{XML_SCHEMA}/types"><TimeZone><Bias>480</Bias><StandardTime><Bias>0</Bias><Time>02:00:00</Time><DayOrder>1</DayOrder><Month>11</Month><DayOfWeek>Sunday</DayOfWeek></StandardTime><DaylightTime><Bias>-60</Bias><Time>02:00:00</Time><DayOrder>2</DayOrder><Month>3</Month><DayOfWeek>Sunday</DayOfWeek></DaylightTime></TimeZone><WorkingPeriodArray><WorkingPeriod><DayOfWeek>Sunday Monday Tuesday Wednesday Thursday Friday Saturday</DayOfWeek><StartTimeInMinutes>0</StartTimeInMinutes><EndTimeInMinutes>1439</EndTimeInMinutes></WorkingPeriod></WorkingPeriodArray></WorkingHours></FreeBusyView></FreeBusyResponse></FreeBusyResponseArray></GetUserAvailabilityResponse></s:Body></s:Envelope>'.format(**XML_KEYS))
+
+ROOM_RESERVE_XML = string.Template('<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="{XML_SOAP_ENVELOPE}"><s:Header><h:ServerVersionInfo xmlns:h="{XML_SCHEMA}/types" xmlns="{XML_SCHEMA}/types" xmlns:xsd="{XML_W3_SCHEMA}" xmlns:xsi="{XML_W3_SCHEMA}-instance" MajorVersion="15" MinorVersion="0" MajorBuildNumber="1210" MinorBuildNumber="2" Version="V2_23"/></s:Header><s:Body xmlns:xsi="{XML_W3_SCHEMA}-instance" xmlns:xsd="{XML_W3_SCHEMA}"><m:CreateItemResponse xmlns:m="{XML_SCHEMA}/messages" xmlns:t="{XML_SCHEMA}/types"><m:ResponseMessages><m:CreateItemResponseMessage ResponseClass="$result"><m:ResponseCode>NoError</m:ResponseCode><m:Items><t:CalendarItem><t:ItemId Id="AAMkADdjOTY0YWI5LWI4NzAtNGU4OC04MjAyLTgyMjFjZmFlMzhkOQBGAAAAAAA537+zip9BToctZGZWgs3IBwD/dt2bgtXgRKEhWftUlaSuAAAC3uxpAADFT7UXKH2LTLWQ1hNAo69lAAC3z5RCAAA=" ChangeKey="DwAAABYAAADFT7UXKH2LTLWQ1hNAo69lAAC34R9T"/></t:CalendarItem></m:Items></m:CreateItemResponseMessage></m:ResponseMessages></m:CreateItemResponse></s:Body></s:Envelope>'.format(**XML_KEYS))
 
 ROOM_EMAIL = 'ROOM@example.com'
 ROOM_NAME = 'ROOM'
-START_TIME = "2016-07-02T11:00:00"
-END_TIME = "2016-07-02T11:00:00"
+START_TIME = "2016-11-09T11:00:00"
+END_TIME = "2016-11-09T11:30:00"
 TIME_ZONE_OFFSET = "480"
-
-
-def get_popen_mock_room_avail(mock_popen, freebusy):
-    process_mock = mock.Mock()
-    attrs = {'communicate.return_value': (ROOM_AVAIL_XML.substitute(freebusy=freebusy), '')}
-    process_mock.configure_mock(**attrs)
-    mock_popen.return_value = process_mock 
 
 @nose.tools.raises(Exception)
 @mock.patch('exchange_api.subprocess.Popen')
 def test_auth_failure(mock_popen):
     get_popen_mock_room_avail(mock_popen, '')
     api = exchange_api.ExchangeApi(user="testuser", password="testpassword")
-    api.find_rooms(prefix="BLAH")
+    api.find_rooms(prefix="ROOM")
     assert False
+
+def get_popen_mock_find_rooms(mock_popen, room_info):
+    process_mock = mock.Mock()
+    attrs = {'communicate.return_value':
+             (FIND_ROOMS_XML.substitute(num_rooms=len(room_info),
+                                        room_info=''.join(room_info)),
+              '')}
+    process_mock.configure_mock(**attrs)
+    mock_popen.return_value = process_mock
+
+@mock.patch('exchange_api.subprocess.Popen')
+def test_find_rooms(mock_popen):
+    ROOM1 = ROOM_INFO_XML.substitute(room_name='ROOM1')
+    ROOM2 = ROOM_INFO_XML.substitute(room_name='ROOM2')
+    ROOM3 = ROOM_INFO_XML.substitute(room_name='ROOM3')
+
+    get_popen_mock_find_rooms(mock_popen, room_info=[ROOM1, ROOM2, ROOM3])
+    api = exchange_api.ExchangeApi(user="testuser", password="testpassword")
+    response = api.find_rooms(prefix="ROOM")
+    assert len(response) == 3
+    assert response.get('ROOM1@example.com')
+    assert response.get('ROOM2@example.com')
+    assert response.get('ROOM3@example.com')
+    assert response.get('ROOM4@example.com') is None
+
+def get_popen_mock_room_avail(mock_popen, freebusy):
+    process_mock = mock.Mock()
+    attrs = {'communicate.return_value': (ROOM_AVAIL_XML.substitute(freebusy=freebusy), '')}
+    process_mock.configure_mock(**attrs)
+    mock_popen.return_value = process_mock
 
 @mock.patch('exchange_api.subprocess.Popen')
 def test_room_available(mock_popen):
@@ -60,12 +92,11 @@ def test_room_unavailable(mock_popen):
     assert response['freebusy'] == FREEBUSY
     assert response['email'] == ROOM_EMAIL
 
-
 def get_popen_mock_room_reserve(mock_popen, result):
     process_mock = mock.Mock()
     attrs = {'communicate.return_value': (ROOM_RESERVE_XML.substitute(result=result), '')}
     process_mock.configure_mock(**attrs)
-    mock_popen.return_value = process_mock 
+    mock_popen.return_value = process_mock
 
 @mock.patch('exchange_api.subprocess.Popen')
 def test_room_reserve_success(mock_popen):
