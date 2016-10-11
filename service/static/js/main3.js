622caafaf9db6c1fe372278a04f17afca625cc70
Saish Gersappa <sgersapp@cisco.com>
2016-10-10 23:28:32 -0700

    sgersapp: Add 30 min intervals
    Change-Id: I1cb697071d692739fd383fb1d2041196dae71948

--- service/static/js/main3.js
+++ service/static/js/main3.js
@@ -3,17 +3,17 @@ var buildings = [];
 var floors = [];;
 var attendees = ["1", "2", "4", "5", "6", "7", "8", "9", "10", "15", "20",
                  "25", "30", "50", "70", "100"];
-var times = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
-             "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
-             "14:00", "15:00", "16:00", "17:00", "18:00", "18:00", "19:00",
-             "20:00", "21:00", "22:00", "23:00"];
-//var times = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00",
-//             "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30",
-//             "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00",
-//             "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
-//             "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00",
-//             "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
-//             "21:00", "21:30", "22:00", "22:30", "23:00", "23:30",];
+//var times = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
+//             "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
+//             "14:00", "15:00", "16:00", "17:00", "18:00", "18:00", "19:00",
+//             "20:00", "21:00", "22:00", "23:00"];
+var times = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00",
+             "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30",
+             "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00",
+             "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
+             "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00",
+             "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
+             "21:00", "21:30", "22:00", "22:30", "23:00", "23:30",];
 var startTimeIndex = null;
 var endTimeIndex = null;
 
@@ -92,13 +92,16 @@ function createTimeRows(data) {
     for (i = 0; i < data.length; i++) {
         tableContainer.innerHTML += "<div class='timeTableRow'>";
         tableContainer.innerHTML += "<label class='timeLabel' id='timeLbl" + i + "'>" + data[i]+"</label>";
-        tableContainer.innerHTML += "<button class='btn btn-default tableButton' id='timeBtn" + i + "' value='" + data[i] + "' onclick='handleTime(" + i + ")'> 1 hour </button>";
+        tableContainer.innerHTML += "<button class='btn btn-default tableButton' id='timeBtn" + i + "' value='" + data[i] + "' onclick='handleTime(" + i + ")'> 30 min </button>";
+        i++;
+        tableContainer.innerHTML += "<label class='timeLabelHidden' id='timeLbl" + i + "'>" + data[i]+"</label>";
+        tableContainer.innerHTML += "<button class='btn btn-default tableButton' id='timeBtn" + i + "' value='" + data[i] + "' onclick='handleTime(" + i + ")'> 30 min </button>";
         tableContainer.innerHTML += "</div>";
     }
 
     tableContainer.innerHTML += "<div class='timeTableRow'>";
-    tableContainer.innerHTML += "<label class='timeLabel' id='timeLbl" + i + "' style='visibility:hidden'>" + data[0]+"</label>";
-    tableContainer.innerHTML += "<button class='btn btn-default tableButton' id='timeBtn" + i + "' value='" + data[0] + "' onclick='handleTime(" + i + ")' style='visibility:hidden'> 1 hour </button>";
+    tableContainer.innerHTML += "<label class='timeLabelHidden' id='timeLbl" + i + "'" + data[0]+"</label>";
+    tableContainer.innerHTML += "<button class='btn btn-default tableButton' id='timeBtn" + i + "' value='" + data[0] + "' onclick='handleTime(" + i + ")' style='visibility:hidden'> 30 min </button>";
     tableContainer.innerHTML += "</div>";
 }
 
@@ -114,10 +117,6 @@ function handleTime(index) {
         startTimeIndex = index;
         endTimeIndex = index;
     }
-    console.log(index + " " + startTimeIndex + " " + endTimeIndex);
-    console.log("start====" +  (eval('timeBtn' + startTimeIndex)).value);
-    console.log("end====" + (eval('timeBtn' + (endTimeIndex + 1))).value);
-    console.log("selected====" + (eval('timeBtn' + index)).value);
 
     var btn;
     var lbl;
